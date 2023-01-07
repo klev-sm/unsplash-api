06ec7f9475cb41090e682774af64f4978a7a9152
Kleverton <tonprofissional9@gmail.com>
2023-01-07 13:13:35 -0300

    Some changes to files

--- app/build/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const App_js_1 = require("./App.js");
-const appConnection = new App_js_1.default();
-const port = appConnection.port;
-appConnection.server.listen(port, () => {
-    console.log(`Successfully running server on port ${port}`);
-});

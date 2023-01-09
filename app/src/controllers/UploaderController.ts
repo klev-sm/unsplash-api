856137af9ead0a8d88668b4609d6d68411b56e25
Kleverton <tonprofissional9@gmail.com>
2023-01-09 13:21:53 -0300

    Uploading and saving to Mongo database

--- app/src/controllers/UploaderController.ts
+++ app/src/controllers/UploaderController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { storage, cloudinary } from "../helpers/uploadSettings.js";
+import { storage, cloudinary } from "../helpers/saveImagesSettings.js";
 import multer = require("multer");
 import { ImageModel } from "../models/ImageModel.js";
 
@@ -7,6 +7,25 @@ const multerUpload = multer({ storage }).single("image");
 
 // Routes Class
 export default class UploaderController {
+    static async getImages(req: Request, res: Response): Promise<void> {
+        try {
+            const images = await ImageModel.find({});
+            if (images) {
+                res.json({
+                    status: "Success on returning images from database.",
+                    images: images,
+                }).status(200);
+            } else {
+                throw new Error(images);
+            }
+        } catch (error) {
+            res.json({
+                status: "Failed to return images",
+                message: error,
+            }).status(404);
+        }
+    }
+
     static saveImage(req: Request, res: Response): void {
         multerUpload(req, res, async function (err) {
             // Treating multer erros
@@ -30,12 +49,12 @@ export default class UploaderController {
                         const cloudinaryUpload =
                             await cloudinary.uploader.upload(savedImage);
                         if (cloudinaryUpload) {
-                            const image = new ImageModel({
+                            const image = await new ImageModel({
                                 link: cloudinaryUpload.secure_url,
-                            });
+                            }).save();
                             if (image) {
                                 res.json({
-                                    status: "Success to upload image to Cloudinary and save it to database.",
+                                    status: "Success on uploading image to Cloudinary and saving it to database.",
                                     file: image.link,
                                 }).status(201);
                             } else {

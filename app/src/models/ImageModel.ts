0d954512b1f746d027e9b4d4c80ad8622fd423c2
Plínio Augusto <plinioaugustoadm@hotmail.com>
2023-01-12 14:51:30 -0300

    comments

--- app/src/models/ImageModel.ts
+++ app/src/models/ImageModel.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-// FIXME neste file voce possui tanto schema quanto models, voce poderia dividir em arquivos diferentes para melhorar a organizacao.
+
 const ImageSchema = new mongoose.Schema({
   link: String,
   publicID: String,
 });
 
-mongoose.set("strictQuery", false); // FIXME Isso aqui é necessario mesmo???
+mongoose.set("strictQuery", false);
 const ImageModel = mongoose.model("images", ImageSchema);
 export { ImageModel };
--- app/src/routes/uploaderRoutes.ts
+++ app/src/routes/uploaderRoutes.ts
@@ -5,8 +5,8 @@ import UploadController from "../controllers/UploaderController.js";
 const uploaderRoutes = Router();
 // routes
 uploaderRoutes
-  .post("/images", UploadController.saveImage)
-  .get("/images", UploadController.getImages)
-  .patch("/images", UploadController.editImage)
-  .delete("/images/:publicID", UploadController.deleteImage);
+    .post("/images", UploadController.saveImage)
+    .get("/images", UploadController.getImages)
+    .patch("/images", UploadController.editImage)
+    .delete("/images/:publicID", UploadController.deleteImage);
 export { uploaderRoutes };
